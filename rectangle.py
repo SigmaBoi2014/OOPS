871b8af93a968b27d9ff3b5a0ce06f762742e573
SigmaBoi2014 <bernardo2014mendes@gmail.com>
2025-06-11 07:55:51 -0700

    Add files via upload

--- rectangle.py
+++ rectangle.py
@@ -21,10 +21,14 @@ class rectangle():
     def draw(self):
         pygame.draw.rect(self.surf,self.color,(self.x,self.y,self.w,self.h),self.t)
 
-r1=rectangle(150,150,0,0,"blue",5)
+r1=rectangle(150,150,0,0,"blue",0)
 r1.draw()
 r2=rectangle(150,100,300,300,"green",0)
 r2.draw()
+r3=rectangle(100,100,150,100,"black",5)
+r3.draw()
+r4=rectangle(100,100,200,200,"red",5)
+r4.draw()
 
 
 
